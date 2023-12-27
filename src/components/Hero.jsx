424531f212841df9d0faf57edd0e5fddb14650cd
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-27 19:37:13 +0530

    bbb

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,14 +9,20 @@ import "swiper/css/navigation";
 // import required modules
 import { Navigation } from "swiper/modules";
 
-const Card = ({ title, subTitle, content }) => {
+const Card = () => {
   return (
-    <div className="">
-      <p>Welcome to Unify Wellness Pvt Ltd</p>
-      <h2>Empowering Health Through Innovative Solutions</h2>
+    <div className=" mt-8">
+      <p className=" text-white text-2xl font-medium capitalize mb-2">Welcome to <span className=" text-[#008EF9]"> Unify Wellness Pvt Ltd </span></p>
+      <h2 className=" text-white font-sfpro text-[50px] font-bold lh-1_1 mb-3">Empowering Health Through Innovative Solutions</h2>
 
-      <p className="text-base opacity-70 text-white">{content}</p>
-      <div>
+      <p className="text-lg opacity-70 text-white mb-4">
+        At Unify Wellness Pvt Ltd, we are dedicated to enhancing your health and
+        wellbeing through innovative healthcare solutions. Our mission is to
+        offer products that blend the latest in wellness technology with
+        everyday usability, ensuring that you experience optimum health benefits
+        seamlessly integrated into your daily life.
+      </p>
+      <div className=" flex  items-center gap-4">
         <a href="/" className="btn-main px-8 py-2">
           Learn More
         </a>
@@ -35,8 +41,8 @@ function Hero() {
   return (
     <div className="hero-bg">
       <div className="container-wrapper">
-        <div className=" py-12 max-w-lg h-full">
-          <Swiper
+        <div className=" py-12 max-w-2xl h-full">
+          {/* <Swiper
             navigation={{
               nextEl: "#nextEl",
               prevEl: "#prevEl",
@@ -56,14 +62,15 @@ function Hero() {
             <SwiperSlide>
             <Card  />
             </SwiperSlide>
-          </Swiper>
+          </Swiper> */}
+          <Card />
         </div>
       </div>
 
-      <div className="flex items-center space-x-5 justify-start">
+      {/* <div className="flex items-center space-x-5 justify-start">
         <SliderNavigationButton id="prevEl" className="rotate-180" />
         <SliderNavigationButton id="nextEl" />
-      </div>
+      </div> */}
     </div>
   );
 }
