f0939952b10f4407965c42b5445b3f2aafdabf82
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-27 19:47:33 +0530

    Update Vision.jsx

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -11,12 +11,18 @@ function Vision() {
             </h1>
           </h1>
 
-          <p className=" text-[#3D4B69] text-center text-base font-normal ">
+          <p className=" text-[#3D4B69] text-center text-base font-normal mx-auto max-w-2xl my-2 ">
             At Unify Wellness Pvt Ltd, we believe in promoting health in a way
             that also respects our planet. We are committed to sustainable
             practices in our production and business operations, ensuring that
             our products are not only good for you but also for the environment.
           </p>
+        </div>
+
+        <div className=" flex items-center justify-between gap-4 my-8">
+          <img src="images/comp1.png" alt="" />
+          <img src="images/comp2.png" alt="" />
+          <img src="images/comp3.png" alt="" />
         </div>
       </div>
     </section>
