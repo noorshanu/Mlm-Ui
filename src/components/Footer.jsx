fdde65b3f1f0e7db66c454b6b794161fb11dd28c
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-27 17:45:18 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,19 +2,71 @@ import React from "react";
 
 function Footer() {
   return (
-    <section className="bg-[#F0FCFF] mt-8 py-10">
-      <div className="container-wrapper">
+    <section className="bg-[#F0FCFF] mt-8 py-6 sm:py-16">
+      <div className="container-wrapper mx-auto">
         <div className=" flex justify-between gap-28">
           <div>
-            <img src="images/logo.png" alt="" />
+            <img src="images/logo.png" alt="" className=" mb-4"/>
 
-            <p>
+            <p className=" max-w-sm">
               We merge the latest in wellness technology with everyday
               usability. Our mission is to provide products that seamlessly
               integrate optimum health benefits into your daily life. Discover a
               holistic approach to well-being with Unify Wellness – your trusted
               partner on the path to a healthier, happier life.
             </p>
+          </div>
+
+          <div>
+            <h2 className=" text-xl text-black font-bold mb-4">Magnetotherapy Biomag</h2>
+
+           <div className=" flex flex-col gap-3 items-start justify-start">
+           <a href="/" className=" text-lg font-normal ">Favourite sets</a>
+            <a href="/" className=" text-lg font-normal ">Applicators</a>
+            <a href="/" className=" text-lg font-normal ">Devices</a>
+            <a href="/" className=" text-lg font-normal ">Accessories</a>
+            <a href="/" className=" text-lg font-normal ">Certificates</a>
+            <a href="/" className=" text-lg font-normal ">Warranty extension 2+1</a>
+            <a href="/" className=" text-lg font-normal ">All about magnetotherapy</a>
+            <a href="/" className=" text-lg font-normal ">3D pulsed magnetotherapy</a>
+           </div>
+
+           
+          </div>
+
+          <div>
+            <h2 className=" text-xl text-black font-bold mb-4">Encyclopedia</h2>
+
+           <div className=" flex flex-col gap-3 items-start justify-start">
+           <a href="/" className=" text-lg font-normal ">Arthrosis of the joints</a>
+            <a href="/" className=" text-lg font-normal ">Carpal tunnel syndrome</a>
+            <a href="/" className=" text-lg font-normal ">Devices</a>
+            <a href="/" className=" text-lg font-normal ">Accessories</a>
+            <a href="/" className=" text-lg font-normal ">Certificates</a>
+            <a href="/" className=" text-lg font-normal ">Warranty extension 2+1</a>
+            <a href="/" className=" text-lg font-normal ">All about magnetotherapy</a>
+            <a href="/" className=" text-lg font-normal ">3D pulsed magnetotherapy</a>
+           </div>
+
+           
+          </div>
+
+
+          <div>
+            <h2 className=" text-xl text-black font-bold mb-4">Magnetotherapy Biomag</h2>
+
+           <div className=" flex flex-col gap-3 items-start justify-start">
+           <a href="/" className=" text-lg font-normal ">Favourite sets</a>
+            <a href="/" className=" text-lg font-normal ">Applicators</a>
+            <a href="/" className=" text-lg font-normal ">Devices</a>
+            <a href="/" className=" text-lg font-normal ">Accessories</a>
+            <a href="/" className=" text-lg font-normal ">Certificates</a>
+            <a href="/" className=" text-lg font-normal ">Warranty extension 2+1</a>
+            <a href="/" className=" text-lg font-normal ">All about magnetotherapy</a>
+            <a href="/" className=" text-lg font-normal ">3D pulsed magnetotherapy</a>
+           </div>
+
+           
           </div>
         </div>
       </div>
