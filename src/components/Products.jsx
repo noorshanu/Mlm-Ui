b31e68e6549466f854984d5097201466148ccc10
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-02 21:22:41 +0530

    new

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -19,20 +19,20 @@ const Cards = ({
 }) => (
   <div className="card-p  ">
     <div>
-      {/* <img src={iconSrc} alt="" /> */}
-      <img src="images/prodimg.png" alt="" />
+      <img src={iconSrc} alt="" className=" mx-auto rounded-lg h-[180px] w-auto sm:w-[266px]" />
+      {/* <img src="images/prodimg.png" alt=""  className=" mx-auto rounded-lg"/> */}
     </div>
     <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <div className=" flex justify-between items-center">
-        <h3>{price}</h3>
-        <p>with GST {gstPrice}</p>
-      </div>
+      <h2 className=" text-black font-sfpro font-medium text-xl mb-1">{title}</h2>
+      <p className=" text-[#979797] text-sm font-sfpro mb-1">{description}</p>
+      {/* <div className=" flex justify-between items-center mb-1">
+        <h3 className="text-[#00AEF3] font-bold text-xl font-sfpro"> ₹ {price}</h3>
+        <p className=" text-[#979797] text-sm font-sfpro ">with GST ₹ {gstPrice}</p>
+      </div> */}
 
       <div className=" flex justify-between items-center">
-        <h3>MRP Price inc GST</h3>
-        <p>{TotalPrice}</p>
+        <h3 className="text-[#979797] text-sm font-sfpro font-normal">MRP Price inc GST</h3>
+        <p className="text-[#000] text-sm font-sfpro font-semibold" >₹ {TotalPrice}  </p>
       </div>
     </div>
   </div>
@@ -51,33 +51,37 @@ function Products() {
         <div className="sm:flex justify-between items-center gap-4 py-4 hidden">
           <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod4.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
-            price="€950.00"
-            gstPrice=" €950.00"
+            price="950.00"
+            gstPrice=" 950.00"
             TotalPrice="7,500.00"
           />
 
           <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod3.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
-            price="€950.00"
-            gstPrice=" €950.00"
+            price="950.00"
+            gstPrice=" 950.00"
             TotalPrice="7,500.00"
           />
 
           <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod2.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
-            price="€950.00"
-            gstPrice=" €950.00"
+            price="950.00"
+            gstPrice=" 950.00"
             TotalPrice="7,500.00"
           />
 
           <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod1.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
-            price="€950.00"
-            gstPrice=" €950.00"
+            price="950.00"
+            gstPrice=" 950.00"
             TotalPrice="7,500.00"
           />
         </div>
@@ -93,6 +97,7 @@ function Products() {
             <SwiperSlide>
             <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod4.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
             price="€950.00"
             gstPrice=" €950.00"
@@ -102,6 +107,7 @@ function Products() {
             <SwiperSlide>
             <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod3.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
             price="€950.00"
             gstPrice=" €950.00"
@@ -111,6 +117,7 @@ function Products() {
             <SwiperSlide>
             <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod2.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
             price="€950.00"
             gstPrice=" €950.00"
@@ -120,6 +127,7 @@ function Products() {
             <SwiperSlide>
             <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod1.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
             price="€950.00"
             gstPrice=" €950.00"
@@ -133,6 +141,7 @@ function Products() {
         <div className=" hidden sm:flex justify-between items-center gap-4 py-4">
           <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod5.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
             price="€950.00"
             gstPrice=" €950.00"
@@ -141,6 +150,7 @@ function Products() {
 
           <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod6.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
             price="€950.00"
             gstPrice=" €950.00"
@@ -149,6 +159,7 @@ function Products() {
 
           <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod7.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
             price="€950.00"
             gstPrice=" €950.00"
@@ -157,6 +168,7 @@ function Products() {
 
           <Cards
             title="Regular Mattress"
+            iconSrc='images/prod/prod8.jpg'
             description="Single Regular Mattress + One Pillow (80+20 Magnets)"
             price="€950.00"
             gstPrice=" €950.00"
