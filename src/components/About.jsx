8d0c6f3fd148bde6a3683e9cd3131295e0fa9fdf
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-08 15:28:14 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,11 +13,11 @@ const BenefitSection = ({ iconSrc, title, description }) => (
 function About() {
   return (
     <section className="py-4 sm:py-14 relative">
-      <img src="images/part2.png" alt="" className=" absolute top-20 h-[230px] sm:h-[537px] " />
+      <img src="images/part2.png" alt="" className=" absolute top-20 h-[230px] sm:h-[529px] " />
 
       <div className="container-wrapper relative z-20">
         <div className="flex justify-between items-center flex-col sm:flex-row gap-6">
-          <img src="images/part1.png" alt="" className=" h-[300px] sm:h-[558px] ml-0 sm:ml-[13%] mt-0 sm:mt-[-5%]" />
+          <img src="images/part1.png" alt="" className=" h-[300px] sm:h-[580px] ml-0 sm:ml-[13%] mt-0 sm:mt-[-5%]" />
           <div> 
             <h1 className=" text-black text-[24px] sm:text-[50px] font-bold my-4 ">Why Bio Magnetic Therapy?</h1>
 
