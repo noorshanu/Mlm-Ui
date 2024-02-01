ad4412b75f73502dfc8043fb74a488d9d4d9bdca
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-02-01 17:19:14 +0530

    Update Terms.jsx

--- src/Pages/Terms.jsx
+++ src/Pages/Terms.jsx
@@ -171,7 +171,8 @@ function Terms() {
               contact us.
             </p>
        
-        <p>Email ID: director@unifywellness.com</p>
+        <p>Email ID: director@unifywellness.co.in</p>
+        <p>Mobile Number:+91 8851924626 </p>
         <p>
           Address: 55,2nd floor, Lane 2 , westend marg ,saidullajab, near saket
           metro station,new delhi ,110030
